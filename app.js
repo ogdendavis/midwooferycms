425b0eea08d4f3f39fe58ee756d3ba6c56c38eeb
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-10-22 10:37:26 -0400

    Install Express

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const http = require('http');
+const express = require('express');
+const app = express();
 
-const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
