0c1abcfd5dbd3e57c429aa36ed60cec1782fc058
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-11-13 13:13:25 -0500

    Add PUT routes to /litters

--- src/routes/litters.js
+++ src/routes/litters.js
@@ -139,19 +139,169 @@ router.post('/', async (req, res) => {
     });
     return res.status(201).send(newLitter);
   } catch (error) {
-    return res
-      .status(500)
-      .send({
-        message: `(Status code ${res.statusCode}) Error creating litter`,
-        error,
-      });
-  }
-  return res
-    .status(500)
-    .send({
+    return res.status(500).send({
       message: `(Status code ${res.statusCode}) Error creating litter`,
-      error: 'unknown',
+      error,
     });
+  }
+  return res.status(500).send({
+    message: `(Status code ${res.statusCode}) Error creating litter`,
+    error: 'unknown',
+  });
 });
+
+// Update a litter by id
+router.put('/:litterId', async (req, res) => {
+  const litterRes = await req.context.models.Litter.findByPk(
+    req.params.litterId
+  );
+  if (!litterRes) {
+    return res
+      .status(404)
+      .send(
+        `(Status code ${res.statusCode}) No litter with ID ${req.litterId}`
+      );
+  }
+
+  // Surface the actual values from the sequelize request
+  const litter = litterRes.dataValues;
+
+  const [badKeys, goodKeys] = [[], []];
+  for (const key in req.body) {
+    if (key === 'id' || !litter.hasOwnProperty(key)) {
+      badKeys.push(key);
+    } else {
+      goodKeys.push(key);
+    }
+  }
+  if (badKeys.length > 0) {
+    return res
+      .status(400)
+      .send(
+        `(Status code ${
+          res.statusCode
+        }) Attempted to update invalid fields: ${badKeys.join(', ')}`
+      );
+  }
+
+  // If bad breederId provided, we'll get a database error due to foreign key constraint
+  if (req.body.breederId) {
+    const breederRes = await req.context.models.Breeder.findByPk(
+      req.body.breederId
+    );
+    if (!breederRes) {
+      return res
+        .status(400)
+        .send(
+          `(Status code ${res.statusCode}) Can't update breederId: No breeder with ID ${req.body.breederId}`
+        );
+    }
+  }
+
+  // Check all provided fields for valid data
+  const invalidFields = [];
+  // Count
+  if (
+    req.body.count &&
+    (typeof req.body.count !== 'number' || req.body.count < 0)
+  ) {
+    invalidFields.push('count');
+  }
+  // Dam
+  if (
+    req.body.dam &&
+    !(await isValidParentData(req.context.models, req.body.dam))
+  ) {
+    invalidFields.push('dam');
+  }
+  // Sire
+  if (
+    req.body.sire &&
+    !(await isValidParentData(req.context.models, req.body.sire))
+  ) {
+    invalidFields.push('sire');
+  }
+  // Pups
+  if (req.body.pups && req.body.pups.constructor !== Array) {
+    invalidFields.push('pups');
+  }
+  // If it is an array, we need to confirm that it contains valid IDs
+  else if (req.body.pups && req.body.pups.length > 0) {
+    if (!(await isValidArrayOfIDs(req.context.models.Dog, req.body.pups))) {
+      invalidFields.push('pups');
+    }
+  }
+
+  if (invalidFields.length > 0) {
+    return res
+      .status(400)
+      .send(
+        `(Status code ${
+          res.statusCode
+        }) Litter not updated: Invalid data provided for fields: ${invalidFields.join(
+          ', '
+        )}`
+      );
+  }
+
+  // Make the changes, and return the updated litter
+  const updatedLitter = await req.context.models.Litter.update(req.body, {
+    where: { id: req.params.litterId },
+    returning: true,
+  });
+
+  // Send what was updated, and the dog info (surfaced)
+  return res
+    .status(200)
+    .send({ updated: goodKeys, result: updatedLitter[1][0] });
+});
+
+// Helper function to confirm sire/dam info is valid
+const isValidParentData = async (models, parent) => {
+  // To confirm that names match, if we get both an ID and a name
+  let confirmedName = false;
+  // Check for type, ensure that it contains ID or name, and nothing else
+  if (
+    parent.constructor !== Object ||
+    (!parent.name && !parent.id) ||
+    !Object.keys(parent).every((i) => ['name', 'id'].includes(i))
+  ) {
+    return false;
+  }
+  // If parent includes ID, make sure it's valid
+  if (parent.id) {
+    const parentRes = await models.Dog.findByPk(parent.id);
+    if (!parentRes) {
+      return false;
+    }
+    confirmedName = parentRes.dataValues.name;
+  }
+  // If have both name and ID, make sure they match
+  if (confirmedName && parent.name && confirmedName !== parent.name) {
+    return false;
+  }
+  // If we get here, we have a name but no ID
+  if (
+    parent.name &&
+    (typeof parent.name !== 'string' || parent.name.length < 2)
+  ) {
+    return false;
+  }
+  return true;
+};
+
+// Helper function to test all elements of pups array
+const isValidArrayOfIDs = async (model, arr) => {
+  for (const i of arr) {
+    if (typeof i !== 'string') {
+      return false;
+    }
+    const res = await model.findByPk(i);
+    if (!res) {
+      return false;
+    }
+  }
+  return true;
+};
 
 export default router;
--- tests/litterRoutes.spec.js
+++ tests/litterRoutes.spec.js
@@ -199,3 +199,239 @@ describe('POST /litters endpoint', () => {
     expect(noInfoRes.text).toEqual(expect.stringContaining('dam'));
   });
 });
+
+/*
+ * PUT
+ */
+
+describe('PUT /litters endpoints', () => {
+  test('Correctly executes valid updates', async () => {
+    // change all the data that we can!
+    const data = {
+      breederId: 'b3',
+      count: 1,
+      dam: { id: 'd1' },
+      sire: { name: 'Andrew' },
+      pups: ['d2'],
+    };
+    const testLitter = utils.randomLitter();
+    const res = await request(app).put(`/litters/${testLitter.id}`).send(data);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.updated).toEqual(Object.keys(data));
+    expect(res.body.result).toEqual({ ...testLitter, ...data });
+  });
+
+  test('Rejects update to non-existent litter', async () => {
+    const data = {
+      id: 'notanid',
+      count: 5,
+    };
+    // Only send the valid update (count) from data
+    const res = await request(app)
+      .put(`/litters/${data.id}`)
+      .send({ count: data.count });
+    expect(res.statusCode).toEqual(404);
+    expect(res.text).toEqual(expect.stringContaining('No litter with ID'));
+  });
+
+  test('Rejects attempt to update ID', async () => {
+    const data = {
+      id: 'shouldntupdate',
+      sire: { name: 'Arthur' },
+    };
+    const testLitter = utils.randomLitter();
+    const res = await request(app).put(`/litters/${testLitter.id}`).send(data);
+    expect(res.statusCode).toEqual(400);
+    expect(res.text).toEqual(expect.stringContaining('id'));
+  });
+
+  test('Rejects updates with non-existent fields', async () => {
+    const data = {
+      does: 'notexist',
+      realField: false,
+    };
+    const testLitter = utils.randomLitter();
+    const res = await request(app).put(`/litters/${testLitter.id}`).send(data);
+    expect(res.statusCode).toEqual(400);
+    expect(res.text).toEqual(expect.stringContaining('does, realField'));
+  });
+
+  test('Rejects update with invalid breederId', async () => {
+    const testLitter = utils.randomLitter();
+    const res = await request(app).put(`/litters/${testLitter.id}`).send({
+      breederId: 'imaginary',
+    });
+    expect(res.statusCode).toEqual(400);
+    expect(res.text).toEqual(expect.stringContaining('No breeder with ID'));
+  });
+
+  test('Rejects update with bad count', async () => {
+    const testLitter = utils.randomLitter();
+    const stringCount = {
+      count: 'iamastring',
+    };
+    // Case: non-number provided for count
+    const stringCountRes = await request(app)
+      .put(`/litters/${testLitter.id}`)
+      .send(stringCount);
+    expect(stringCountRes.statusCode).toEqual(400);
+    expect(stringCountRes.body).toEqual({});
+    expect(stringCountRes.text).toEqual(expect.stringContaining('count'));
+    // Case: negative number provided for count
+    const negCount = {
+      count: -1,
+    };
+    const negCountRes = await request(app)
+      .put(`/litters/${testLitter.id}`)
+      .send(negCount);
+    expect(negCountRes.statusCode).toEqual(400);
+    expect(negCountRes.body).toEqual({});
+    expect(negCountRes.text).toEqual(expect.stringContaining('count'));
+    // Confirm that test litter hasn't changed
+    const getRes = await request(app).get(`/litters/${testLitter.id}`);
+    expect(getRes.statusCode).toEqual(200);
+    expect(getRes.body).toEqual(testLitter);
+  });
+
+  test('Rejects update with bad dam info', async () => {
+    const testLitter = utils.randomLitter();
+    // Case: Dam is not an object
+    const stringDam = {
+      dam: 'stringy',
+    };
+    const stringDamRes = await request(app)
+      .put(`/litters/${testLitter.id}`)
+      .send(stringDam);
+    expect(stringDamRes.statusCode).toEqual(400);
+    expect(stringDamRes.body).toEqual({});
+    expect(stringDamRes.text).toEqual(expect.stringContaining('dam'));
+    // Case: Dam object contains invalid ID
+    const badId = {
+      dam: { id: 'notanid' },
+    };
+    const badIdRes = await request(app)
+      .put(`/litters/${testLitter.id}`)
+      .send(badId);
+    expect(badIdRes.statusCode).toEqual(400);
+    expect(badIdRes.body).toEqual({});
+    expect(badIdRes.text).toEqual(expect.stringContaining('dam'));
+    // Case: Dam object contains invalid name
+    const badName = {
+      dam: { name: 'a' },
+    };
+    const badNameRes = await request(app)
+      .put(`/litters/${testLitter.id}`)
+      .send(badName);
+    expect(badNameRes.statusCode).toEqual(400);
+    expect(badNameRes.body).toEqual({});
+    expect(badNameRes.text).toEqual(expect.stringContaining('dam'));
+    // Case: Dam objects contains both name and ID, but they don't match
+    const noMatch = {
+      dam: {
+        id: utils.randomDog({ sex: 'f' }).id,
+        name: '***There is no dog with this name***',
+      },
+    };
+    expect(badNameRes.statusCode).toEqual(400);
+    expect(badNameRes.body).toEqual({});
+    expect(badNameRes.text).toEqual(expect.stringContaining('dam'));
+    // Case: Dam object contains neither name nor ID
+    const noIdOrName = {
+      dam: { quest: 'make_puppies' },
+    };
+    const noIdOrNameRes = await request(app)
+      .put(`/litters/${testLitter.id}`)
+      .send(noIdOrName);
+    expect(noIdOrNameRes.statusCode).toEqual(400);
+    expect(noIdOrNameRes.body).toEqual({});
+    expect(noIdOrNameRes.text).toEqual(expect.stringContaining('dam'));
+    // Confirm that test litter hasn't changed
+    const getRes = await request(app).get(`/litters/${testLitter.id}`);
+    expect(getRes.statusCode).toEqual(200);
+    expect(getRes.body).toEqual(testLitter);
+  });
+
+  test('Rejects update with bad sire info', async () => {
+    const testLitter = utils.randomLitter();
+    // Case: Sire is not an object
+    const stringSire = {
+      sire: 'stringy',
+    };
+    const stringSireRes = await request(app)
+      .put(`/litters/${testLitter.id}`)
+      .send(stringSire);
+    expect(stringSireRes.statusCode).toEqual(400);
+    expect(stringSireRes.body).toEqual({});
+    expect(stringSireRes.text).toEqual(expect.stringContaining('sire'));
+    // Case: Sire object contains bad ID
+    const badId = {
+      sire: { id: 'notanid' },
+    };
+    const badIdRes = await request(app)
+      .put(`/litters/${testLitter.id}`)
+      .send(badId);
+    expect(badIdRes.statusCode).toEqual(400);
+    expect(badIdRes.body).toEqual({});
+    expect(badIdRes.text).toEqual(expect.stringContaining('sire'));
+    // Case: Sire object contains bad name
+    const badName = {
+      sire: { name: 999 },
+    };
+    const badNameRes = await request(app)
+      .put(`/litters/${testLitter.id}`)
+      .send(badName);
+    expect(badNameRes.statusCode).toEqual(400);
+    expect(badNameRes.body).toEqual({});
+    expect(badNameRes.text).toEqual(expect.stringContaining('sire'));
+    // Case: Sire object contains neither ID nor name
+    const noIdOrName = {
+      sire: { quest: 'make_puppies' },
+    };
+    const noIdOrNameRes = await request(app)
+      .put(`/litters/${testLitter.id}`)
+      .send(noIdOrName);
+    expect(noIdOrNameRes.statusCode).toEqual(400);
+    expect(noIdOrNameRes.body).toEqual({});
+    expect(noIdOrNameRes.text).toEqual(expect.stringContaining('sire'));
+    // Confirm that test litter hasn't changed
+    const getRes = await request(app).get(`/litters/${testLitter.id}`);
+    expect(getRes.statusCode).toEqual(200);
+    expect(getRes.body).toEqual(testLitter);
+  });
+
+  test('Rejects updates with bad pup info', async () => {
+    const testLitter = utils.randomLitter();
+    // Case: Pups is not an array
+    const stringPup = { pups: 'ishouldbeanarray' };
+    const stringPupRes = await request(app)
+      .put(`/litters/${testLitter.id}`)
+      .send(stringPup);
+    expect(stringPupRes.statusCode).toEqual(400);
+    expect(stringPupRes.body).toEqual({});
+    expect(stringPupRes.text).toEqual(expect.stringContaining('pups'));
+    // Case: Pups array doesn't contain all valid IDs
+    const badId = {
+      pups: ['d1', 'pups', 'must_have', 'only', 'valid_ids', 'd2'],
+    };
+    const badIdRes = await request(app)
+      .put(`/litters/${testLitter.id}`)
+      .send(badId);
+    expect(badIdRes.statusCode).toEqual(400);
+    expect(badIdRes.body).toEqual({});
+    expect(badIdRes.text).toEqual(expect.stringContaining('pups'));
+    // Confirm that test litter hasn't changed
+    const getRes = await request(app).get(`/litters/${testLitter.id}`);
+    expect(getRes.statusCode).toEqual(200);
+    expect(getRes.body).toEqual(testLitter);
+  });
+});
+
+/*
+ * DELETE
+ */
+
+describe('DELETE /litters endpoint', () => {
+  test('Removes litter entirely', async () => {});
+
+  test('Fails if given bad ID', async () => {});
+});
