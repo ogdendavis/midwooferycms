e5321575b2f84c35317f99488086857332fbd2a3
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-11-14 10:00:14 -0500

    Additional test util tweaks

--- tests/setup/utils.js
+++ tests/setup/utils.js
@@ -12,18 +12,37 @@ const utils = {
       deletedAt: expect.notUndefined(),
     };
   },
-  randomBreeder(args = { hasDogs: false }) {
-    if (args.hasDogs) {
-      const breederId = this.randomDog().breederId;
-      const breeder = breeders.filter((b) => b.id === breederId)[0];
-      return this.dataize(breeder);
+
+  randomBreeder(args = { hasDogs: null }) {
+    if (args.hasDogs !== null) {
+      const idsWithDogs = [
+        ...new Set(
+          this.allDogs()
+            .map((d) => d.breederId)
+            .filter((i) => {
+              return i.length > 0;
+            })
+        ),
+      ];
+      const breedersWithDogs = breeders.filter((b) =>
+        idsWithDogs.includes(b.id)
+      );
+      const breedersWithoutDogs = breeders.filter(
+        (b) => !idsWithDogs.includes(b.id)
+      );
+      return this.dataize(
+        this.randomFromArray(
+          args.hasDogs ? breedersWithDogs : breedersWithoutDogs
+        )
+      );
     }
     return this.dataize(this.randomFromArray(breeders));
   },
   allBreeders() {
     return breeders.map((b) => this.dataize(b));
   },
-  randomLitter(args = { hasPups: false }) {
+
+  randomLitter(args = { hasPups: null }) {
     if (args.hasPups) {
       let testId = this.randomDog().litterId;
       // If dog has no litter listed, litterId will be empty string
@@ -33,11 +52,22 @@ const utils = {
       const litter = litters.filter((l) => l.id === testId)[0];
       return this.dataize(litter);
     }
+    if (args.hasPups === false) {
+      return this.dataize(
+        this.randomFromArray(litters.filter((l) => l.pups.length === 0))
+      );
+    }
     return this.dataize(this.randomFromArray(litters));
   },
-  allLitters() {
+  allLitters(args = { breederId: false }) {
+    if (args.breederId) {
+      return litters
+        .filter((l) => l.breederId === args.breederId)
+        .map((fl) => this.dataize(fl));
+    }
     return litters.map((l) => this.dataize(l));
   },
+
   randomDog(args = { sex: false }) {
     if (args.sex) {
       return this.randomFromArray(dogs.filter((d) => d.sex === args.sex));
--- tests/utils.spec.js
+++ tests/utils.spec.js
@@ -8,3 +8,39 @@ test('randomDog respects sex argument', () => {
   expect(male.sex).toEqual('m');
   expect(female.sex).toEqual('f');
 });
+
+test('randomLitter respects hasPups argument', () => {
+  const noPups = utils.randomLitter({ hasPups: false });
+  const yesPups = utils.randomLitter({ hasPups: true });
+  expect(noPups.pups).toEqual([]);
+  expect(yesPups.pups.length).toBeGreaterThan(0);
+});
+
+test('randomBreeder respects hasDogs argument', () => {
+  // Remember, all dogs have breeders, but not all breeders have dogs
+  const noDogs = utils.randomBreeder({ hasDogs: false });
+  const yesDogs = utils.randomBreeder({ hasDogs: true });
+  const breedersWithDogs = [
+    ...new Set(
+      utils
+        .allDogs()
+        .map((d) => d.breederId)
+        .filter((i) => i.length > 0)
+    ),
+  ];
+  expect(breedersWithDogs).toContainEqual(yesDogs.id);
+  expect(breedersWithDogs).not.toContainEqual(noDogs.id);
+});
+
+test('allLitters respects breederId argument', () => {
+  const testBreederId = utils.randomLitter().breederId;
+  const allLitters = utils.allLitters();
+  const testLitters = utils.allLitters({ breederId: testBreederId });
+  // Expect that all returned litters should have correct breeder id
+  for (const l of testLitters) {
+    expect(l.breederId).toEqual(testBreederId);
+  }
+  // Expect that all litters with a different breeder plus all litters with this breeder should equal the total number of litters
+  const otherLitters = allLitters.filter((l) => l.breederId !== testBreederId);
+  expect(otherLitters.length + testLitters.length).toEqual(allLitters.length);
+});
